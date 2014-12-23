e479108b8ba9a657771ac1f6d7e45b86d51eec73
Raymond Tang <raymond@tang.me>
2014-12-23 18:55:05 -0500

    fix typo

--- finger.py
+++ finger.py
@@ -8,7 +8,7 @@ def click():
     # Range is from 500 to 2400
     # Swing for clicking is alternating from 1200 to 1800.
     global clicks
-    clicks++
+    clicks += 1
     if clicks%2 == 0:
         servo.set_servo(18, 1200)
     else:
