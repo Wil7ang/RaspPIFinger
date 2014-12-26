1c6f852acd6e39cd33c3f0270b30dfe32f2c4d15
Wilson Tang <Wil.7ang@gmail.com>
2014-12-25 19:30:15 -0500

    Test circle drawing

--- imagingTest.py
+++ imagingTest.py
@@ -18,6 +18,7 @@ columnNumberForPipe = 80
 while(True):
     ret,frame = camera.read()
     frame = cv2.cvtColor(frame,cv2.cv.CV_BGR2GRAY)
+    print frame[columnNumberForPipe][np.argmin(frame[columnNumberForPipe])]
     # print frame[columnNumberForPipe][220]
     # for x in range(220,60,-1):
     #     if(inRange(frame[columnNumberForPipe][x][0],140, 0) and \
