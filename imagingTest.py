174e1c881631627d1952bfc8de4f0189dfc65424
Raymond Tang <raymond@tang.me>
2014-12-25 21:12:06 -0500

    use rolling window for bird detection

--- imagingTest.py
+++ imagingTest.py
@@ -15,6 +15,15 @@ max_height = 220
 columnNumberForPipe = 80
 bird_col = 195
 
+bird_window = []
+
+def rolling_window(location):
+    global bird_window
+    bird_window.append(location)
+    if len(bird_window) > 5:
+        bird_window.pop(0)
+    return sum(bird_window[-5:])/5.0
+
 while(True):
     ret,frame = camera.read()
     frame = cv2.cvtColor(frame,cv2.cv.CV_BGR2GRAY)
@@ -22,7 +31,7 @@ while(True):
     blackLoc = 80 + np.argmin(frame[columnNumberForPipe][80:max_height])
     cv2.circle(frame,(blackLoc,columnNumberForPipe),2,(0,0,255,255),2)
 
-    bird_loc = min_height + np.argmin(frame[bird_col][min_height:max_height])
+    bird_loc = rolling_window(min_height + np.argmin(frame[bird_col][min_height:max_height]))
     cv2.circle(frame,(bird_loc, bird_col), 2, (0,255,255,255), 2)
 
     cv2.imshow('Frame',frame)
