6a4f300621a66032b34211d192d4c8122f76ddcb
Raymond Tang <raymond@tang.me>
2014-12-25 20:18:06 -0500

    del unused code

--- imagingTest.py
+++ imagingTest.py
@@ -1,9 +1,6 @@
 import cv2
 import numpy as np
 
-import cv2
-import numpy as np
-
 def inRange(val,max,min):
     return val < max and val > min
 
@@ -18,21 +15,14 @@ columnNumberForPipe = 80
 while(True):
     ret,frame = camera.read()
     frame = cv2.cvtColor(frame,cv2.cv.CV_BGR2GRAY)
-    #print frame[columnNumberForPipe][np.argmin(frame[columnNumberForPipe])]
+
     blackLoc = 80 + np.argmin(frame[columnNumberForPipe][80:220])
     cv2.circle(frame,(blackLoc,columnNumberForPipe),2,(0,0,255,255),2)
-    # print frame[columnNumberForPipe][220]
-    # for x in range(220,60,-1):
-    #     if(inRange(frame[columnNumberForPipe][x][0],140, 0) and \
-    #        inRange(frame[columnNumberForPipe][x][1],140, 0) and \
-    #        inRange(frame[columnNumberForPipe][x][2],140, 0)):
-    #         cv2.circle(frame,(x,columnNumberForPipe),2,(0,0,255,255),2)
-    #         break
-
 
     cv2.imshow('Frame',frame)
 
     if(cv2.waitKey(1) == ord('q')):
         break
+
 camera.release()
 cv2.destroyAllWindows()
