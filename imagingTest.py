66298cfbd6babe19167ac3785747f3a70e773109
Wilson Tang <Wil.7ang@gmail.com>
2014-12-25 19:03:10 -0500

    Test circle drawing

--- imagingTest.py
+++ imagingTest.py
@@ -19,12 +19,12 @@ while(True):
     ret,frame = camera.read()
     # print frame[columnNumberForPipe][197]
     for x in range(220,0,-1):
-        if(inRange(frame[columnNumberForPipe][x][0], 90, 50) and \
+        if(inRange(frame[columnNumberForPipe][x][0], 90, 40) and \
            inRange(frame[columnNumberForPipe][x][1],230,190) and \
            inRange(frame[columnNumberForPipe][x][2],230,190)):
+            cv2.circle(frame,(x,columnNumberForPipe),2,(0,0,255,255),2)
             continue
         else:
-            cv2.circle(frame,(x,columnNumberForPipe),2,(0,0,255,255),2)
             break
             #cv2.circle(frame,(x,columnNumberForPipe),2,(0,0,255,255),2)
             #break            
