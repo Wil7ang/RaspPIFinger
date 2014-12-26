d3ef7f50b9bc60896c4fb2b1f8018fe3155f6234
Wilson Tang <Wil.7ang@gmail.com>
2014-12-25 20:21:03 -0500

    Test circle drawing

---
+++ imagingTest.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+def inRange(val,max,min):
+    return val < max and val > min
+
+camera = cv2.VideoCapture(0)
+camera.set(3,320)
+camera.set(4,240)
+
+cv2.namedWindow('Frame')
+
+columnNumberForPipe = 80
+
+while(True):
+    ret,frame = camera.read()
+    frame = cv2.cvtColor(frame,cv2.cv.CV_BGR2GRAY)
+
+    blackLoc = 90 + np.argmin(frame[columnNumberForPipe][90:220])
+    cv2.circle(frame,(blackLoc,columnNumberForPipe),2,(0,0,255,255),2)
+
+    cv2.imshow('Frame',frame)
+
+    if(cv2.waitKey(1) == ord('q')):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
