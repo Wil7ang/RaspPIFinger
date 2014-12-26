7325988ed1125a6baa38cb59a5cfe85602147b4e
Wilson Tang <Wil.7ang@gmail.com>
2014-12-25 20:59:30 -0500

    Test circle drawing

--- imagingTest.py
+++ imagingTest.py
@@ -21,7 +21,8 @@ while(True):
     args = args[::-1]
 
     blackLoc = 90 + args[0]#np.argmin(frame[columnNumberForPipe][90:220])
-    cv2.circle(frame,(blackLoc,columnNumberForPipe),2,(0,0,255,255),2)
+    if(blackLoc >95):
+        cv2.circle(frame,(blackLoc,columnNumberForPipe),2,(0,0,255,255),2)
 
     cv2.imshow('Frame',frame)
 
