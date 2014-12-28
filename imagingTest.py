a4593759cd429797271517a1a5ae96bb89d9abd4
Wilson Tang <Wil.7ang@gmail.com>
2014-12-28 17:09:57 -0500

    Test circle drawing

--- imagingTest.py
+++ imagingTest.py
@@ -49,9 +49,9 @@ while(True):
         #cv2.circle(frame,(blackLoc,columnNumberForPipe),2,(0,0,255,255),2)
         # pipeArray.append([blackLoc,columnNumberForPipe])
 
-    for index in [pipe in pipeArray where pipe[1] > 240]:
+    for index in [pipe in pipeArray if pipe[1] > 240]:
         pipeArray.pop(index)
-        
+
     for pipe in pipeArray:
         cv2.circle(frame,(pipe[0],pipe[1]),2,(0,0,255,255),2)
         pipe[1] = pipe[1] + 4
