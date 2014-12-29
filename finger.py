65d191bd6d3cb68effc100b13968d0fc3c09e91c
Raymond Tang <raymond@tang.me>
2014-12-28 19:27:53 -0500

    merge/functionalize imagingTest

--- finger.py
+++ finger.py
@@ -12,11 +12,52 @@ cv2.namedWindow('Frame')
 
 min_height = 20
 max_height = 220
-columnNumberForPipe = 80
+pipe_col = 80
 bird_col = 195
+pipes = []
+frames_seen = 0
+last_loc = 0
+catch_time = dt.datetime.now()
+start_time = dt.datetime.now()
+target_height = 160
 
 def detect_bird(frame):
     return min_height + np.argmin(frame[bird_col][min_height:max_height])
+
+
+def detect_pipe(frame):
+    args = np.argsort(frame[pipe_col][90:220])[0:5]
+    args = np.sort(args)[::-1]
+    return 90 + args[0]
+
+
+def set_target_range(frame, bird_loc, pipe_loc):
+    global last_loc, target_height
+    if (dt.datetime.now() - start_time).microseconds/1000 > 150:
+        if(grey[pipe_col][pipe_loc] < 100 and frames_seen == 0):
+            last_loc = pipe_loc
+            catch_time = dt.datetime.now()
+            frames_seen += 1
+        elif frames_seen > 0:
+            if abs(pipe_loc - last_loc) < 7:
+                frames_seen += 1
+            else:
+                frames_seen = 0
+
+        if frames_seen >= 3:
+            pipes.append([last_loc, pipe_col, catch_time, False])
+            frames_seen = 0
+            start_time = dt.datetime.now()
+
+    for index, pip in enumerate([pipe for pipe in pipes if
+                                 (dt.datetime.now() - pipe[2]).microseconds/1000 > 800]):
+        pipes.pop(index)
+
+    for pipe in pipes:
+        if(not pipe[3] and (dt.datetime.now() - pipe[2]).microseconds/1000 > 400):
+            pipe[3] = True
+            target_height = pipe[0] - 15
+            break
 
 
 JUMP_HEIGHT = 30
@@ -37,10 +78,17 @@ def click():
 
 def main():
     while(True):
-        ret,frame = camera.read()
-        frame = cv2.cvtColor(frame, cv2.cv.CV_BGR2GRAY)
+        ret, frame = camera.read()
+        grey = cv2.cvtColor(frame, cv2.cv.CV_BGR2GRAY)
 
-        detect_bird(frame)
+        bird_loc = detect_bird(grey)
+        pipe_loc = detect_pipe(grey)
+
+        set_target_range(grey, bird_loc, pipe_loc)
+
+        cv2.circle(frame, (target_height, 195), 2, (0, 0, 255, 255), 2)
+        cv2.line(frame, (target_height, 0), (target_height, 240), (0, 0, 255, 255), 2)
+        cv2.line(frame, (target_height-75, 0), (target_height-75, 240), (0, 0, 255, 255), 2)
 
         cv2.imshow('Frame', frame)
 
